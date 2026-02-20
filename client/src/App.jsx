c6861926df7f6cfdaa9faf5800b881c59bf1144d
chrisnordrum <nordrum.chris@gmail.com>
2026-02-20 11:25:47 -0700

    Reconfigure React Router imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
