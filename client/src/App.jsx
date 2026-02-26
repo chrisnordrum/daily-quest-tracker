3cb258a88772d7e74d67ed815ca1b07ee9806ccf
Diane Schultze <rosemariediane.schultze@edu.sait.ca>
2026-02-25 21:45:55 -0700

    Route pages & nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Nav from "./components/Nav.jsx";
+import Guild from "./pages/Guild";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
     <>
+      <Nav />
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/guild" element={<Guild />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      
     </>
   )
 }
