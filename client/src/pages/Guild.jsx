f5c3d3e1833b8ade90b1b59b9052873a530e1bbc
Diane Schultze <rosemariediane.schultze@edu.sait.ca>
2026-02-27 19:40:25 -0700

    Remove H1 inline styling

--- client/src/pages/Guild.jsx
+++ client/src/pages/Guild.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function Guild() {
     return (
         <div>
-        <h1 className="text-center mt-10 text-purple-400">Guild Page</h1>
+        <h1 className="text-center mt-10">Guild Page</h1>
         </div>
     )
 }
--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Guild() {
     return (
         <div>
-            <h1 className="text-center mt-10 text-pink-400">Leaderboard</h1>
+            <h1 className="text-center mt-10">Leaderboard</h1>
         </div>
     )
 }
