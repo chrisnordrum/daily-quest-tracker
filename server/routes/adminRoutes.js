87d26162ae8a960ad169d216d06f9a52ef5e7fb7
Diane Schultze <rm.dianeschultze@gmail.com>
2026-03-20 20:17:16 -0600

    Testing

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,7 +4,16 @@ const authMiddleware = require("../middleware/auth");
 const auth = require("../middleware/authorize");
 
 router.get("/", authMiddleware, auth("admin"), (req, res) => {
-  res.status(200).json({ message: "Welcome, admin user!" });
+  res.status(200).json({
+    message: "Welcome, admin user!",
+    adminFeatures: [
+      "Manage users",
+      "View reports",
+      "Add Quests",
+      "Edit Quests",
+      "Delete Quests",
+    ]
+  });
 });
 
 module.exports = router;
