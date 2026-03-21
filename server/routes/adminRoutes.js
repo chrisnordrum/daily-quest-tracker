7c5bb9f324bdcde981844db862761fcbb22f3c28
Diane Schultze <rm.dianeschultze@gmail.com>
2026-03-21 09:56:59 -0600

    Okay putting it back to this

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,18 +2,23 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth");
 const auth = require("../middleware/authorize");
+const User = require("../models/User");
 
 router.get("/", authMiddleware, auth("admin"), (req, res) => {
   res.status(200).json({
-    message: "Welcome, admin user!",
-    adminFeatures: [
-      "Manage users",
-      "View reports",
-      "Add Quests",
-      "Edit Quests",
-      "Delete Quests",
-    ]
+    message: "Welcome to the admin dashboard!",
   });
+});
+
+router.get("/users", authMiddleware, auth("admin"), async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Error fetching users" });
+  }
 });
 
 module.exports = router;
