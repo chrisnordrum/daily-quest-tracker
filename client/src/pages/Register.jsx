b6baf47f496b979a8e0587916f44bfffac3800b1
CanadaPlayz48 <radznag3354@gmail.com>
2026-03-21 18:35:34 -0600

    Bug fix attempt for SSO

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -43,7 +43,7 @@ export default function Register() {
     if (!window.google || googleInitialized.current) return;
 
     window.google.accounts.id.initialize({
-      client_id: import.meta.env.GOOGLE_CLIENT_ID,
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
       callback: handleGoogleCredentialResponse,
     });
 
