7826ac790857207167257e7523f1e51745a004a4
Chris Nordrum <chrisnordrum@proton.me>
2026-03-22 18:10:49 -0600

    Comment out dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 "use strict";
-require("dotenv").config();
+//require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
