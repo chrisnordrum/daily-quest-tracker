5a72abcb14edfc11de8bf204fa75a55ecf1f2170
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-08 15:18:00 -0600

    add bio to res when user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -141,6 +141,7 @@ const login = async (req, res) => {
         email: user.email,
         first_name: user.first_name,
         last_name: user.last_name,
+        bio: user.bio,
       },
     });
   } catch (error) {
