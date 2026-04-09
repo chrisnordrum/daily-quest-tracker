e959df23930f08eb8ac24753820e5df282914204
Diane Schultze <rm.dianeschultze@gmail.com>
2026-04-09 16:22:49 -0600

    just fix z-index

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -295,7 +295,7 @@ export default function Profile() {
       </div>
 
       {openUpdateModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-20">
+        <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-card p-6 rounded-3xl w-full max-w-lg border-2 border-border">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-semibold">Update Profile</h2>
