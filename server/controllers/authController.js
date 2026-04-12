0ecfe915bc0683cb591fd53ba0fc078f13d17235
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-12 16:33:46 -0600

    fix bugs when user modify profile and then get info

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -292,10 +292,10 @@ const modifyProfile = async (req, res) => {
 
     const responseUser = {
       username: user.username,
-      email: user.email,
+      email,
       first_name: user.first_name,
       last_name: user.last_name,
-      bio: user.bio,
+      bio,
     };
 
     res
